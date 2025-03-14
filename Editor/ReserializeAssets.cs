3ebf8e014139a67e6861f79878c17cd3369cf391
fresolina <74408325+fresolina@users.noreply.github.com>
2025-03-14 14:12:51 +0100

    feat: add asset reserialization functionality to context menu and selection

--- Editor/ReserializeAssets.cs
+++ Editor/ReserializeAssets.cs
@@ -6,10 +6,25 @@ namespace Lotec.Utils {
         static string[] _paths = new string[1];
 
         [MenuItem("Tools/Lotec/Reserialize assets")]
-        static void DoIt() {
-            // _paths[0] = AssetDatabase.GetAssetPath(Selection.activeObject);
-            // AssetDatabase.ForceReserializeAssets(_paths);
+        static void ReserializeAllAssets() {
             AssetDatabase.ForceReserializeAssets();
+        }
+
+        // Reserialize the asset right-clicked at
+        [MenuItem("Tools/Lotec/Reserialize selected asset")]
+        static void ReserializeAsset() {
+            ReserializeObject(Selection.activeObject);
+        }
+
+        // Add Reserialize to context menu
+        [MenuItem("Assets/Reserialize", false, 0)]
+        static void ReserializeAssetFromContextMenu() {
+            ReserializeObject(Selection.activeObject);
+        }
+
+        static void ReserializeObject(Object obj) {
+            _paths[0] = AssetDatabase.GetAssetPath(obj);
+            AssetDatabase.ForceReserializeAssets(_paths);
         }
     }
 }
