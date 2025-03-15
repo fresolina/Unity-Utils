4e5058a1f4703b288e38d041aff0957028dd90fb
fresolina <74408325+fresolina@users.noreply.github.com>
2025-03-15 10:11:03 +0100

    refactor: optimize EmojiIconManager for static usage and initialization

--- Runtime/EmojiIconManager.cs
+++ Runtime/EmojiIconManager.cs
@@ -6,42 +6,42 @@ namespace Lotec.Utils {
     /// <summary>
     /// Converts emoji names to unicode characters.
     /// Usage: EmojiIconManager.GetEmoji("grinning face");
-    /// Data source:
-    /// https://github.com/muan/unicode-emoji-json/blob/main/data-by-group.json
+    /// Data source: https://github.com/muan/unicode-emoji-json/blob/main/data-by-group.json
     /// Example emoji font: https://github.com/googlefonts/noto-emoji
     /// </summary>
-    public class EmojiIconManager : MonoBehaviour2 {
-        static EmojiIconManager s_instance;
-        readonly Dictionary<string, string> _emojiFromName = new Dictionary<string, string>();
+    public static class EmojiIconManager {
+        static readonly Dictionary<string, string> s_emojiFromName = new Dictionary<string, string>();
+        static bool s_isInitialized;
 
-        public static string GetEmoji(string name) => s_instance.GetEmojiFromText(name);
-        public string GetEmojiFromText(string name) => _emojiFromName.TryGetValue(name.ToLower(), out var emoji) ? emoji : string.Empty;
-
-        void Awake() {
-            if (s_instance == null) {
-                s_instance = this;
-            }
-            LoadEmojiData();
+        static EmojiIconManager() {
+            Initialize();
         }
 
-        bool LoadEmojiData() {
-            // Load json file with emoji mappings
+        public static string GetEmoji(string name) {
+            if (!s_isInitialized)
+                Initialize();
+            return s_emojiFromName.TryGetValue(name.ToLower(), out var emoji) ? emoji : string.Empty;
+        }
+
+        static void Initialize() {
+            if (s_isInitialized)
+                return;
+
             var json = Resources.Load<TextAsset>("data-by-group");
             if (json == null) {
                 Debug.LogError("Emoji data not found: Need data-by-group.json in Resources folder.");
-                return false;
+                return;
             }
             // Hack to fix json format. TODO: Add utility to download file + fix asset instead.
             string jsonText = "{\"groups\":" + json.text + "}";
             EmojiGroupList list = JsonUtility.FromJson<EmojiGroupList>(jsonText);
-            // Remap emoji names to unicode
             foreach (var group in list.groups) {
                 foreach (var emoji in group.emojis) {
-                    _emojiFromName[emoji.name.ToLower()] = emoji.emoji;
+                    s_emojiFromName[emoji.name.ToLower()] = emoji.emoji;
                 }
             }
 
-            return true;
+            s_isInitialized = true;
         }
 
         /// <summary>
